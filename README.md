425cb52e0072a3eb141c42a00bc2413769855e84
Asier Lara <asier.laraechebarrieta.ext@telefonica.com>
2023-09-06 17:01:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# View Transition
+
+Technologies
+
+- **[Astro](https://astro.build/)**
+
+Run
+
+- _npm install_
+- _npm run dev_
---
+++ src/data/pokemon.ts
@@ -0,0 +1,102 @@
+export const pokemons = [
+  {
+    id: crypto.randomUUID(),
+    name: 'bulbasaur',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'ivysaur',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'venusaur',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'charmander',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'charmeleon',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'charizard',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'squirtle',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'wartortle',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'blastoise',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'caterpie',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'metapod',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/11.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'butterfree',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/12.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'weedle',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/13.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'kakuna',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/14.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'beedrill',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/15.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'pidgey',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/16.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'pidgeotto',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'pidgeot',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/18.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'rattata',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png'
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'raticate',
+    url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/20.png'
+  }
+]
